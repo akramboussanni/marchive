1ce84f351575f5ff2d7b217e4da4fdeda8d6e0e6
akramboussanni <akramboussanni@gmail.com>
2025-08-29 22:33:04 -0400

    ratelimit improvements

--- internal/api/routes/books/routes.go
+++ internal/api/routes/books/routes.go
@@ -30,21 +30,30 @@ func NewBookRouter(repos *repo.Repos) http.Handler {
 	r.Use(middleware.MaxBytesMiddleware(1 << 20))
 
 	r.Group(func(r chi.Router) {
-		middleware.AddRatelimit(r, 30, 1*time.Minute)
+		middleware.AddRatelimit(r, 100, 1*time.Minute)
 		r.Get("/explore", br.HandleExplore)
+	})
+
+	r.Group(func(r chi.Router) {
+		middleware.AddRatelimit(r, 50, 1*time.Minute)
 		r.Get("/job/{jobID}", br.HandleJobStatus)
 		r.Get("/{hash}/download", br.HandleDownloadFile)
 	})
 
 	r.Group(func(r chi.Router) {
-		middleware.AddRatelimit(r, 10, 1*time.Minute)
+		middleware.AddRatelimit(r, 30, 1*time.Minute)
 		middleware.AddAuth(r, repos.User, repos.Token)
 		r.Post("/search", br.HandleSearch)
+		r.Get("/downloads", br.HandleUserDownloads)
+		r.Get("/favorites", br.HandleGetFavorites)
+	})
+
+	r.Group(func(r chi.Router) {
+		middleware.AddRatelimit(r, 15, 1*time.Minute)
+		middleware.AddAuth(r, repos.User, repos.Token)
 		r.Post("/download", br.HandleRequestDownload)
 		r.Post("/download/cached", br.HandleCachedDownloadRequest)
-		r.Get("/downloads", br.HandleUserDownloads)
 		r.Post("/favorite", br.HandleToggleFavorite)
-		r.Get("/favorites", br.HandleGetFavorites)
 	})
 
 	return r
